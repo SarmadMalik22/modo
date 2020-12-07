c47a9ac1c6fae2935c54f3d0bdfdb208071e1b63
SarmadMalik22 <lazymalik22@gmail.com>
2020-12-07 01:55:18 -0800

    Initial commit

---
+++ extra_addons/hospital/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Hospital Management',
+    'version' : '1.1',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'https://www.hospital.com/',
+    'depends' : [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ extra_addons/hospital/models/patient.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', required=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'other'),
+    ], required=True, default='male')
+    note = fields.Text(string='Description')
